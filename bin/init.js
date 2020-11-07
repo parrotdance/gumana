7436ff4861d5af7ee8a485a76a313d1c0d34abf9
ParroT <parrotdance95@gmail.com>
2020-11-07 20:59:40 +0800

    make response log pretty

--- bin/init.js
+++ bin/init.js
@@ -2,11 +2,15 @@ const fs = require('fs-extra')
 const { execSync } = require('child_process')
 const {
   SELF_CFG_PATH,
+  SELF_CFG_DIR,
   VERSION,
   getCurrentUser,
   question,
   formatUserInfo,
-  addUserPreset
+  addUserPreset,
+  logSucceed,
+  log,
+  logWarn
 } = require('../src/utils')
 const options = require('../src/entries')
 
@@ -17,7 +21,7 @@ const gitExist = () => {
 const checkCfg = () => fs.statSync(SELF_CFG_PATH).size > 0
 const welcome = `
 Thanks for using gumana.
-But we can not find .guconfig at: ${SELF_CFG_PATH}
+We use config file at: ${SELF_CFG_DIR}, but we can not find it.
 Would you like to create this config file automaticly? (y/n) `
 
 module.exports = async function init() {
@@ -43,14 +47,15 @@ module.exports = async function init() {
         const { name, email } = getCurrentUser()
         const userInfo = formatUserInfo(name, email)
         addUserPreset(userInfo)
-        console.log(`Current user info has been stored: ${userInfo}`)
-        console.log(`\nNext:\n`)
-        console.log(`  Please run 'gumana -a' to add a new git user preset.`)
-        console.log(`  Then, run 'gumana' again to switch git user.`)
+        log('...\n')
+        logSucceed(`Done. Now gumana is ready to go.`)
+        log(`\nNext:\n`)
+        log(`  Please run 'gumana -a' to add a new git user preset.`)
+        log(`  Then, run 'gumana' again to switch git user.`)
         process.exit(0)
       } else if (opt === 'n') {
         fs.removeSync(SELF_CFG_PATH)
-        console.log('Negative. Process exit.')
+        logWarn('Negative. Process exit.')
         process.exit(-1)
       }
     })
--- src/handlers/reset-handler.js
+++ src/handlers/reset-handler.js
@@ -1,15 +1,16 @@
 const fs = require('fs-extra')
-const { question, SELF_CFG_PATH } = require('../utils')
+const { question, SELF_CFG_PATH, logWarn, logSucceed } = require('../utils')
 
 module.exports = async function resetHandler() {
   const exist = fs.existsSync(SELF_CFG_PATH)
   if (exist) {
-    const questStr = `Reset gumana will delete file: ${SELF_CFG_PATH}.\nWhich will lose *ALL OF* git user presets. Are you sure? (y/n)`
-    await question(questStr, (ans) => {
+    const warning = `Reset gumana will delete file: ${SELF_CFG_PATH}.\n  Which will lose *ALL OF* git user presets.`
+    logWarn(warning)
+    await question(`Are you sure? (y/n) `, (ans) => {
       if (ans === 'y') {
         try {
           fs.unlinkSync(SELF_CFG_PATH)
-          console.log(`File has been deleted: ${SELF_CFG_PATH}`)
+          logSucceed(`File has been deleted: ${SELF_CFG_PATH}`)
         } catch (e) {
           console.error(e)
         }
